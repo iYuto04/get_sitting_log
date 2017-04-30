a2deb86dcad0d363852257ab863ff15340f58636
Yuto Mizutani <i.yuto.curiosity@gmail.com>
2017-04-30 16:39:28 +0900

    v1.0完成

--- get_data.py
+++ get_data.py
@@ -1,15 +1,23 @@
 import fitbit
-from config import fitbit_cfg
+# from config import fitbit_cfg
+import sys, os
+import datetime
+sys.path.append(os.getcwd() + '/python-fitbit/')
+import gather_keys_oauth2
+
 
 def sitting_time():
+    fitbit_cfg = gather_keys_oauth2.write_config_file()
     CLIENT_ID = fitbit_cfg['client_id']
     CLIENT_SECRET = fitbit_cfg['client_secret']
     ACCESS_TOKEN = fitbit_cfg['access_token']
     REFRESH_TOKEN = fitbit_cfg['refresh_token']
     authd_client = fitbit.Fitbit(CLIENT_ID, CLIENT_SECRET,
     access_token = ACCESS_TOKEN, refresh_token = REFRESH_TOKEN)
-    rsp = authd_client.intraday_time_series('activities/minutesSedentary')
-    return rsp['activities-minutesSedentary'][0]['value']
+    yesterday = datetime.date.today() + datetime.timedelta(days=-1)
+    week_ago = yesterday + datetime.timedelta(days=-6)
+    rsp = authd_client.time_series('activities/minutesSedentary',base_date=week_ago,end_date=yesterday)
+    return [int(data['value']) for data in rsp['activities-minutesSedentary']]
 
 if __name__ == '__main__':
     print(sitting_time())
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from post_slack import Danpei
-import datetime
+from get_data import sitting_time
+from plot_graph import plot_week_log
+from post_slack import post_graph
 
-def check_data():
-    d = datetime.datetime.today()
-    today = '{0}-{1}-{2}'.format(d.year, d.month, d.day)
+def main():
+    data = sitting_time()
+    plot_week_log(data)
+    post_graph()
+
 if __name__ == '__main__':
-    check_data()
+    main()
